a8b3db0abe05f20bbf4b22d9ed92a1ad4f89d8f4
Iris Law <irislawly@gmail.com>
2021-04-09 21:24:20 -0700

    pushe

---
+++ app/src/main/java/com/bcit/game/MainActivity.java
@@ -0,0 +1,29 @@
+package com.bcit.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.bcit.game.AudioCall;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.buttonTTT);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TTT.class));
+                //
+            }
+        });
+        AudioCall ac;
+    }
+}
---
+++ app/src/main/java/com/bcit/game/TTT.java
@@ -0,0 +1,16 @@
+package com.bcit.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TTT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t_t_t);
+        //https://owlcation.com/stem/tictactoe
+        //https://www.simplifiedcoding.net/tic-tac-toe-android-app-tutorial/
+    }
+}
